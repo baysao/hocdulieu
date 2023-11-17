856157bb7d241d15ee7b5f9515698d8c16bc474b
baysao <baysao@TS04013s-MacBook-Pro.local>
2023-11-17 16:30:42 +0700

    remove other items

--- app/model/side_menu.js
+++ app/model/side_menu.js
@@ -6,15 +6,15 @@ define([], function () {
       icon: "mdi mdi-home",
     },
     // { id: "app", value: "App Detail", icon: "mdi mdi-code-json" },
-    { id: "explorer", value: "Explorer", icon: "mdi mdi-math-log" },
-    { id: "composer", value: "Composer", icon: "mdi mdi-pencil" },
-    { id: "mempool", value: "Mempool", icon: "mdi mdi-lan-connect" },
-    { id: "notify", value: "Notify", icon: "mdi mdi-map-marker-alert" },
-    {
-      id: "developer",
-      value: "Developer",
-      icon: "mdi mdi-file-code-outline",
-    },
+    // { id: "explorer", value: "Explorer", icon: "mdi mdi-math-log" },
+    // { id: "composer", value: "Composer", icon: "mdi mdi-pencil" },
+    // { id: "mempool", value: "Mempool", icon: "mdi mdi-lan-connect" },
+    // { id: "notify", value: "Notify", icon: "mdi mdi-map-marker-alert" },
+    // {
+    //   id: "developer",
+    //   value: "Developer",
+    //   icon: "mdi mdi-file-code-outline",
+    // },
   ];
   return _data;
 });
